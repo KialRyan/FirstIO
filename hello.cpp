21bd50f390ec1cbd5d4a55566d05430aa0178145
KialRyan <kleiteryan@mail.csuchico.edu>
2018-01-29 17:38:11 -0800

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,9 @@
+//Author:Kial//
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
-    std::cin >> input;
-    std::cout << "Hello, " << input << std::endl;
+    std::cout << "Hi, my name is Kial" << std::endl;
     return 0;
 }
